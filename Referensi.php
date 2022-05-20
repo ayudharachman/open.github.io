2ea8f0d8fd297ee4b4411dd89424f6248530a4f0
ayudharachman <ayudharachman@gmail.com>
2022-05-20 09:31:46 +0700

    Create Referensi.php

---
+++ Referensi.php
@@ -0,0 +1,27 @@
+<?php
+
+    function _getdoa(){
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://doa-doa-api-ahmadramadhan.fly.dev/api/doa/pergi',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Origin: *',
+            'Access-Control-Allow-Origin: *'
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
+    }
+
+?>
